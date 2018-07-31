356b91f59f8f29e0b89c92b9ac3c95e668d29186
raj-singh-2018 <41345691+raj-singh-2018@users.noreply.github.com>
2018-08-01 01:09:21 +0530

    Create Reversal algorithm.js

---
+++ Reversal algorithm.js
@@ -0,0 +1,23 @@
+// bake your code here
+
+
+function reversearray(a, start, stop){
+    while(start < stop){
+        let temp = a[start]
+        a[start] = a[stop]
+        a[stop] = temp
+        start++
+        stop--
+    }
+}
+
+function reversalalgo(a, d){
+    reversearray(a, 0, d-1)
+    reversearray(a, d, a.size - 1)
+    reversearray(a, 0, a.size - 1)
+    
+}
+
+let a = [1, 2, 3, 4, 5, 6, 7]
+reversalalgo(a, 2)
+print(a)
